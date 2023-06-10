af80c9f9a768c270df05a8447586acc325eec6be
Pichaya Chantrasriwong <pichaya8442@gmail.com>
2023-06-11 02:39:51 +0700

    finish navbar

---
+++ script.js
@@ -0,0 +1,32 @@
+const open_nav = document.getElementById('open-nav');
+const close_nav = document.getElementById('close-nav');
+const nav_item = document.getElementById('nav-item');
+const profile_nav = document.getElementById('profile-nav')
+
+const main = document.getElementById('main');
+const login = document.getElementById('login');
+
+if(open_nav){
+    open_nav.addEventListener('click',()=>{
+        nav_item.classList.add('nav_active')
+        main.classList.add('main_not_active')
+        document.body.style.backdropFilter = 'blur(24px)';
+    })
+}
+
+
+if(close_nav){
+    close_nav.addEventListener('click',()=>{
+        nav_item.classList.remove('nav_active')
+        main.classList.remove('main_not_active')
+        document.body.style.backdropFilter = 'none';
+    })
+}
+
+if(profile_nav){
+    profile_nav.addEventListener('click',()=>{
+        login.classList.add('login_active')
+    })
+}
+
+// .style.backgroundColor =
