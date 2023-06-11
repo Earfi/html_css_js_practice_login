d44dea95a6e7791917aab772a7bc76e2796bd5d7
Pichaya Chantrasriwong <pichaya8442@gmail.com>
2023-06-11 17:36:18 +0700

    login form

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const open_nav = document.getElementById('open-nav');
 const close_nav = document.getElementById('close-nav');
+const close_form = document.getElementById('close-form');
 const nav_item = document.getElementById('nav-item');
 const profile_nav = document.getElementById('profile-nav')
 
 const main = document.getElementById('main');
 const login = document.getElementById('login');
+
+
 
 if(open_nav){
     open_nav.addEventListener('click',()=>{
@@ -28,5 +31,13 @@ if(profile_nav){
         login.classList.add('login_active')
     })
 }
+
+if(close_form){
+    close_form.addEventListener('click',()=>{
+        login.classList.remove('login_active')
+    })
+}
+
+
 
 // .style.backgroundColor =
